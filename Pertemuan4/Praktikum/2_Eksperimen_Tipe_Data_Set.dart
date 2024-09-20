c13445f3d31f2aacbf3ddb8b83d64d6a47f0d790
daffayudisaa <daffayudisa09@gmail.com>
2024-09-20 10:35:27 +0700

    fix: Praktikum 2 Jobsheet 4

--- Pertemuan4/Praktikum/2_Eksperimen_Tipe_Data_Set.dart
+++ Pertemuan4/Praktikum/2_Eksperimen_Tipe_Data_Set.dart
@@ -4,14 +4,17 @@ void main() {
 
   var names1 = <String>{};
   Set<String> names2 = {}; // This works, too.
-  var names3 = <String>{};
+  //var names3 = <String>{};
   // var names3 = {}; // Creates a map, not a set.
 
   names1.add("Daffa Yudisa Akbar");
   names2.add("2241720008");
-  names3.addAll({...names1, ...names2});
+
+  names1.addAll({"Budi", "Doni", "Malik"});
+  names2.addAll({"2241720001", "2241720002", "2241720003"});
+  //names3.addAll({...names1, ...names2});
 
   print(names1);
   print(names2);
-  print(names3);
+  //print(names3);
 }
